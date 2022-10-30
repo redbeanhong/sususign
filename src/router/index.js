6a78902c24e5c79e37a1e66450f3d8a0f7aea961
RedBean <myself6331@gmail.com>
2022-10-30 14:52:20 +0800

    2022.10.30 [環境]建置基本分頁
    1.移除vite產生的既有分頁
    2.新增首頁、文件、PDF編輯、設定頁面
    3.新增nav
    4.調整global.scss至main.js全域

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/document",
+      name: "document",
+      component: () => import("../views/DocumentView.vue"),
+    },
+    {
+      path: "/config",
+      name: "config",
+      component: () => import("../views/ConfigView.vue"),
+    },
+    {
+      path: "/pdf_edit",
+      name: "pdf_edit",
+      component: () => import("../views/PdfEditView.vue"),
     },
   ],
 });
