a50e2297c9d66e8aa3650aaba91c7a2f06389d68
RedBean <myself6331@gmail.com>
2022-11-17 20:43:24 +0800

    2022.11.17 [新增]nav、footer、icon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <script setup>
 import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
+import FooterBar from "./components/FooterBar.vue";
 </script>
 
 <template>
-  <header>
-    <NavBar />
-  </header>
-  <main>
+  <main id="main">
+    <header>
+      <NavBar />
+    </header>
     <RouterView />
   </main>
+  <footer id="footer">
+    <FooterBar />
+  </footer>
 </template>
+
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-between;
+}
+
+#main,
+#footer {
+  width: 100%;
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,10 +2,29 @@
 import { RouterLink } from "vue-router";
 </script>
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/document">Document</RouterLink>
-    <RouterLink to="/config">Config</RouterLink>
-    <RouterLink to="/pdf_edit">PdfEdit</RouterLink>
+  <nav id="nav">
+    <div class="container">
+      <div class="navbar">
+        <RouterLink to="/"><h1>數速簽</h1></RouterLink>
+        <div class="links">
+          <RouterLink to="/">
+            <img src="@/assets/img/icon/iconUpload.png" alt="Upload" />
+            上傳
+          </RouterLink>
+          <RouterLink to="/document">
+            <img src="@/assets/img/icon/iconFile.png" alt="File" />
+            文件
+          </RouterLink>
+          <RouterLink to="/config">
+            <img src="@/assets/img/icon/iconSetting.png" alt="Setting" />
+            設定
+          </RouterLink>
+          <RouterLink to="/pdf_edit">
+            <img src="@/assets/img/icon/iconUpload.png" alt="Upload" />
+            PdfEdit
+          </RouterLink>
+        </div>
+      </div>
+    </div>
   </nav>
 </template>
